807b64fc2c2c61547ca7c8e86c9c55b344138efe
coffeewine <52611996+coffeewine@users.noreply.github.com>
2021-12-27 19:50:27 -0300

    Create pomfsup.sh

---
+++ pomfsup.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+#license: MIT
+
+#another pomf uploader
+##no multiplr files support
+##no upload via url support
+
+host="$1"
+localFile="$2"
+
+if [ "$#" -lt 2 ] || [ "$#" -gt 2 ]; then
+
+echo "
+Pomf file uploader
+Usage: ./$(basename "$0") alias File
+
+Supported sites:
+  * pomf.cat     [75 MiB]       (alias: pomfcat)
+  * uguu.se      [128 MiB 48h]  (alias: uguu)
+  * catbox.moe   [200 MiB]      (alias: catbox)
+  * 0x0.st       [512 MiB]      (alias: 0x0)
+  * zz.ht        [1024 MiB]     (alias: zzht)
+  * cockfile.com [2048 MiB 24H] (alias: cock)
+"
+exit
+fi
+
+case "$host" in
+    pomfcat) hostURL='https://pomf.cat/upload.php' ;;
+    uguu) hostURL='https://uguu.se/api.php?d=upload-tool' ;;
+    0x0) hostURL='https://0x0.st' ;;
+    catbox) hostURL='https://catbox.moe/user/api.php' ;;
+    zzht) hostURL='https://zz.ht/api/upload' ;;
+    cock) hostURL='https://cockfile.com/upload.php' ;;
+esac
+
+if [ "$host" = 'uguu' ]; then
+    uploadResult="$(curl -sf -F file="@$localFile" "$hostURL")"
+
+elif [ "$host" = 'pomfcat' ]; then
+    uploadResult="$(curl -sf -F files[]=@"$localFile" "$hostURL" | jq -c -r --arg base "https://a.pomf.cat/" '$base + .files[].url')"
+
+elif [ "$host" = '0x0' ]; then
+	uploadResult="$(curl -sf -F file="@$localFile" "$hostURL")"
+
+elif [ "$host" = 'catbox' ]; then
+    uploadResult="$(curl -sf -F reqtype="fileupload" -F userhash="" -F fileToUpload="@$localFile" "$hostURL")"
+
+elif [ "$host" = 'zzht' ]; then
+    uploadResult="$(curl -sf -F DestinationType="FileUploader" -F files[]=@"$localFile" "$hostURL" | jq -r .files[].url)"
+
+elif [ "$host" = 'cock' ]; then
+    uploadResult="$(curl -sf -F files[]=@"$localFile" "$hostURL" | egrep -o "a.cockfile.com[a-z0-9A-Z./\]{1,20}" | sed 's/\\//')"
+
+else
+    echo "something went wrong"
+fi
+
+echo "$uploadResult"
